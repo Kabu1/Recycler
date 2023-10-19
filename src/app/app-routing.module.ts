866a2378aac419ebba3c6c376218a8e79b050841
kabu1 <bensonmk99@gmail.com>
2023-10-19 12:52:19 -0400

    create the pickup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'pickup',
+    loadChildren: () => import('./pages/pickup/pickup.module').then( m => m.PickupPageModule)
   }
 ];
 
