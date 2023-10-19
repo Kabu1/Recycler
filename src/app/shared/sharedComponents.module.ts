ded48fd68d7859ea3debe655217007a20b215982
kabu1 <bensonmk99@gmail.com>
2023-10-19 18:31:37 -0400

    create a reusable component for the pickup cards

---
+++ src/app/shared/sharedComponents.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { PickupCallCardComponent } from '../components/pickup-call-card/pickup-call-card.component';
+
+@NgModule({
+  declarations: [PickupCallCardComponent],
+  exports: [PickupCallCardComponent],
+  
+})
+export class SharedModule {}
