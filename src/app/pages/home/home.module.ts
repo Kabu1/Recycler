8b728f592749498252a07428364017fec1738e14
kabu1 <bensonmk99@gmail.com>
2023-10-19 19:26:42 -0400

    add shared module to consume pickupcallcard resusable component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,15 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
-import { PickupCallCardComponent } from 'src/app/components/pickup-call-card/pickup-call-card.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SharedModule
   ],
-  declarations: [HomePage, PickupCallCardComponent]
+  declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PickupCallCardComponent } from '../components/pickup-call-card/pickup-call-card.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [PickupCallCardComponent],
   exports: [PickupCallCardComponent],
-  
+  imports: [IonicModule]
 })
 export class SharedModule {}
+
