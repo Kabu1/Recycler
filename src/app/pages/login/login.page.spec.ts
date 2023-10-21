871093541049709d34ea34373e3ce9c52fd02302
kabu1 <bensonmk99@gmail.com>
2023-10-21 01:15:16 -0400

    email and phone validators

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { LoginPage } from './login.page';
 import { Router } from '@angular/router';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -11,7 +12,7 @@ describe('LoginPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [LoginPage],
-      imports: [AppRoutingModule]
+      imports: [AppRoutingModule, ReactiveFormsModule]
     }).compileComponents().then(() => {
       fixture = TestBed.createComponent(LoginPage);
       router = TestBed.get(Router)
@@ -19,6 +20,10 @@ describe('LoginPage', () => {
       fixture.detectChanges();
     });
   }));
+  it('should create form on init', () => {
+    component.ngOnInit();
+    expect(component.form).not.toBeUndefined();
+ })
   it('should go to home page on login', ()=>{
     spyOn(router, 'navigate');
     component.login();
