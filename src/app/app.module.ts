9464012b73f36bdcc2f84245bc74d41e43fb27a2
kabu1 <bensonmk99@gmail.com>
2023-10-22 18:37:32 -0400

    setup ngrx store state, action and reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
+import { AppStoreModule } from './store/appStoreModule';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule, CommonModule
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SharedModule,
+    CommonModule,
+    AppStoreModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
