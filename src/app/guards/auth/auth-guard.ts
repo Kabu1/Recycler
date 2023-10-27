729261f7832be9d1cd1f46bca33db56e9f6bf7ea
kabu1 <bensonmk99@gmail.com>
2023-10-27 16:34:03 -0400

    authguard cleanup

--- src/app/guards/auth/auth-guard.ts
+++ src/app/guards/auth/auth-guard.ts
@@ -1,9 +1,38 @@
+// import { Injectable } from '@angular/core';
+// import { CanLoad, Router } from '@angular/router';
+// import { AppState } from '@capacitor/app';
+// import { Store } from '@ngrx/store';
+// import { Observable, of } from 'rxjs';
+// import {  switchMap, take } from 'rxjs/operators';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuard implements CanLoad {
+
+//   constructor(private store: Store<AppState>, private router: Router) { }
+
+//   canLoad(): Observable<boolean>{
+//     return this.store.select('login').pipe(
+//       take(1), 
+//       switchMap(loginState =>{
+//         if(loginState.isLoggedIn){
+//           return of(true);
+//         }
+//         this.router.navigateByUrl('login');
+//         return of(false);
+
+//       })
+//     )
+//   }
+// }
+
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
 import { AppState } from '@capacitor/app';
 import { Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
-import {  switchMap, take } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +41,19 @@ export class AuthGuard implements CanLoad {
 
   constructor(private store: Store<AppState>, private router: Router) { }
 
-  canLoad(): Observable<boolean>{
-    return this.store.select('login').pipe(
-      take(1), 
-      switchMap(loginState =>{
-        if(loginState.isLoggedIn){
-          return of(true)
+  canLoad(): Observable<boolean> {
+    return this.store.select(state => state.login).pipe(
+      take(1),
+      switchMap(loginState => {
+        if (loginState.isLoggedIn) {
+          return of(true);
         }
         this.router.navigateByUrl('login');
-        return of(false)
-
+        return of(false);
       })
-    )
+    );
   }
 }
+
 
 
