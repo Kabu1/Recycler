8b15ef618e91ee2bf4622b71ce95970ad50b0ae0
kabu1 <bensonmk99@gmail.com>
2023-10-30 18:24:21 -0400

    add www folder and index for the capacitor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
-export const environment = {
-  production: true
-};
+  export const environment = {
+    firebaseConfig: {
+      apiKey: "AIzaSyBda99s6XDmftz_DYM1Lu6mbpPzekIpZdc",
+      authDomain: "recycler-1caf6.firebaseapp.com",
+      projectId: "recycler-1caf6",
+      storageBucket: "recycler-1caf6.appspot.com",
+      messagingSenderId: "90678409642",
+      appId: "1:90678409642:web:dce3e678fffa71626ad3d3"
+    },
+    production: true;
+  };
+  
